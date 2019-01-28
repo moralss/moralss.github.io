6b378aa0053d3a5e6805723803c7590027cb54c6
moralss <jeramoral@gmail.com>
2019-01-28 10:55:41 +0200

    added my games to my github pages

---
+++ pomodro-clock/mainPomodro.js
@@ -0,0 +1,91 @@
+var SessionTime = 1500;
+var breakTime = 0000;
+var SessionInterval;
+var breakInterval;
+
+updateClockDisplay("SessionOutput", SessionTime);
+updateClockDisplay("breakOutput", breakTime);
+styleElement(["completeSession"], "hidden");
+
+function addSessionBreakTime() {
+    breakTime += 60;
+    updateClockDisplay("breakOutput", breakTime);
+}
+
+function addSessionTime() {
+    SessionTime += 60;
+    updateClockDisplay("SessionOutput", SessionTime);
+};
+
+function subtactSessionTime() {
+    if (SessionTime >= 1) {
+        SessionTime -= 60;
+        updateClockDisplay("SessionOutput", SessionTime);
+    };
+};
+
+
+function subtactBreakTime() {
+    if (breakTime >= 1) {
+        breakTime -= 60;
+        updateClockDisplay("breakOutput", breakTime);
+    };
+};
+
+function resetPomodroClock() {
+    location.reload();
+}
+
+function BreakDecreaseTimer() {
+    breakTime--;
+    if (breakTime <= 0) {
+        clearInterval(breakInterval);
+        styleElement(["completeSession"], "visible");
+        styleElement(["breakOutput", "BreakTimeLabel"], "hidden");
+    };
+
+    updateClockDisplay("breakOutput", breakTime);
+}
+
+function SessionDecreaseTimer() {
+    SessionTime--;
+    if (SessionTime <= 0) {
+        clearInterval(SessionInterval);
+        breakInterval = setInterval(BreakDecreaseTimer, 1000);
+        styleElement(["SessionOutput", "SessionTimeLabel"], "hidden");
+        styleElement(["breakOutput", "BreakTimeLabel"], "visible");
+    };
+
+    updateClockDisplay("SessionOutput", SessionTime);
+}
+
+function styleElement(array, action) {
+    return array.forEach(element => document.getElementById(element).style.visibility = action);
+}
+
+function hiddenElement() {
+    styleElement(["BreakTimeLabel", "SessionTimeLabel"], "hidden");
+}
+
+function startCountDown() {
+
+    styleElement(["subtractBreak", "setSession", "setBreak", "SubtractSession", "addSession", "addBreak", "breakOutput"], "hidden");
+    styleElement(["SessionTimeLabel"], 'visible');
+    SessionInterval = setInterval(SessionDecreaseTimer, 1000);
+    
+};
+
+function updateClockDisplay(display, counter) {
+    var minutes = Math.floor(counter / 60);
+    var seconds = counter % 60;
+    var formattedSeconds = seconds >= 10 ? seconds : "0" + seconds;
+    document.getElementById(display).innerHTML = minutes + ":" + formattedSeconds;
+};
+
+
+
+
+
+
+
+
