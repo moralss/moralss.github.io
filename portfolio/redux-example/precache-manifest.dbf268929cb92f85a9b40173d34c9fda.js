e2d1bf89ed1c2ab39ad6738ff3a0cb090c16dd66
moralss <jeramoral@gmail.com>
2019-08-22 09:20:02 +0200

    pushing an example

---
+++ portfolio/redux-example/precache-manifest.dbf268929cb92f85a9b40173d34c9fda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a0123aee8e574acc4a89e000a5c480",
+    "url": "/portfolio/redux-example/index.html"
+  },
+  {
+    "revision": "67c47624aba471f5b1ae",
+    "url": "/portfolio/redux-example/static/css/main.65fd01e2.chunk.css"
+  },
+  {
+    "revision": "d7db72bf32653c5acac6",
+    "url": "/portfolio/redux-example/static/js/2.f57a9422.chunk.js"
+  },
+  {
+    "revision": "67c47624aba471f5b1ae",
+    "url": "/portfolio/redux-example/static/js/main.4088362a.chunk.js"
+  },
+  {
+    "revision": "c2856c0ed22874299a57",
+    "url": "/portfolio/redux-example/static/js/runtime~main.1e497306.js"
+  }
+]);
